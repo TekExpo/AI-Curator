f9f4a33096740ecb30911c66e996aba0407a8f85
Abhijeet Jadhav <jeet.m.jadhav@gmail.com>
2026-03-13 20:27:14 -0400

    Updated

--- SharePointWebPart/lib-commonjs/webparts/aiCuratorArticleRecommender/AiCuratorArticleRecommender.config.js
+++ SharePointWebPart/lib-commonjs/webparts/aiCuratorArticleRecommender/AiCuratorArticleRecommender.config.js
@@ -1,8 +1,2 @@
 "use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.aiCuratorArticleRecommenderConfig = void 0;
-exports.aiCuratorArticleRecommenderConfig = {
-    llmEndpointUrl: 'https://devuser1-webinardemo-resource.cognitiveservices.azure.com/openai/deployments/gpt-4o/chat/completions?api-version=2025-01-01-preview',
-    openAiApiKey: '2s13yNIRq2IY8DcjwRAocRanLSwOkyLzAhEh9ETW8MmOMEqTQGM2JQQJ99BGACHYHv6XJ3w3AAAAACOGJc1T'
-};
 //# sourceMappingURL=AiCuratorArticleRecommender.config.js.map
